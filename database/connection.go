7a3df34c8d8444d19d114084f4d819f2e0d64ee3
lucassmann <lucas.assmann@edu.pucrs.br>
2022-06-05 19:04:08 -0300

    created DB variable for the database connection

--- database/connection.go
+++ database/connection.go
@@ -7,12 +7,16 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 	connection, err := gorm.Open(mysql.Open("root:rootroot@/go_auth"), &gorm.Config{})
 
 	if err != nil {
 		panic("could not connector to the database")
 	}
+
+	DB = connection
 
 	connection.AutoMigrate(&models.User{})
 }
